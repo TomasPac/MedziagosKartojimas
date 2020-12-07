8cf410bf977361a8bb0b4e68a6a697eb632a7f52
TomasPac <tompacesa@gmail.com>
2020-12-07 20:46:31 +0200

    init

---
+++ MedziagosKartojimas/BaseClass.cs
@@ -0,0 +1,15 @@
+﻿namespace MedziagosKartojimas
+{
+    abstract class BaseClass
+    {
+        private readonly string name;
+
+        public BaseClass(string Name)
+        {
+            name = Name;
+        }
+        public abstract string NameMetodas();
+
+        public abstract int NameMetodas(int a);
+    }
+}
