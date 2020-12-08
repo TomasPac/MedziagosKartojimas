53065df39d05db3119a5e086ddb2fd7a3412c418
TomasPac <tompacesa@gmail.com>
2020-12-08 19:19:31 +0200

    IZodisKitaip interface

---
+++ MedziagosKartojimas/IZodisKitaip.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas
+{
+    public interface IZodisKitaip
+    {
+        public string ZodisBeBalsiu();
+        public string ZodisBePriebalsiu();
+        public string ZodisSuPakeistomisBalsemis(char ch);
+        public string ZodisSuPakeistomisPriebalsemis(int a);
+    }
+}
