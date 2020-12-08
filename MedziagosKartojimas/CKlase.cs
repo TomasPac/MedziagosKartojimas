09a789904fee9044efde541f25ab103336f6a355
TomasPac <tompacesa@gmail.com>
2020-12-08 20:08:38 +0200

    task 5

---
+++ MedziagosKartojimas/CKlase.cs
@@ -0,0 +1,30 @@
+﻿namespace MedziagosKartojimas
+{
+    public class CKlase : BKlase
+    {
+
+        public int Skaicius { get; set; }
+        public string Zodis { get; set; }
+        public double DoubleSkaicius { get; set; }
+        public AKlase AObjektas { get; set; }
+        public CKlase(string name) : base(name)
+        {
+
+        }
+        public CKlase(string name, int skaicius) :base(name)
+        {
+            Skaicius = skaicius;
+        }
+        public CKlase(string name, int skaicius, string zodis) : base(name)
+        {
+            Skaicius = skaicius;
+            Zodis = zodis;
+        }
+        public CKlase(string name, int skaicius, string zodis, AKlase aObjektas) : base(name)
+        {
+            Skaicius = skaicius;
+            Zodis = zodis;
+            AObjektas = aObjektas;
+        }
+    }
+}
